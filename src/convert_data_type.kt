97093350d050c6ea7da82743f9a7aeefbc936c86
Fachri Naufal <fachrinfl@gmail.com>
2019-03-26 17:53:27 +0700

    convert data type

---
+++ src/convert_data_type.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var age:Int = readLine()!!.toInt()
+
+    var name:String = "25071995"
+    name.toInt()
+}
---
+++ src/declaring_variables.kt
@@ -0,0 +1,20 @@
+fun main (args: Array<String>) {
+    // var and val
+    // var is mutable
+    // val is immutable
+
+//    var name = "Fachri Naufal"
+//    var age = 24
+//
+//    println("My name is " + name + ", I\'m " + age + " old")
+
+    var name:String = "Fachri"
+    var age:Int = 23
+
+    var programmingFavorite:String?
+    programmingFavorite = null
+    programmingFavorite = "Kotlin"
+
+    var myAge:Int?
+    myAge = null
+}
