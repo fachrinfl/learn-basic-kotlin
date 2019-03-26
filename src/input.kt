cc35c111bc97ee2983ed3c1332dc48fc6388a7f5
Fachri Naufal <fachrinfl@gmail.com>
2019-03-26 16:21:30 +0700

    input user

---
+++ src/input.kt
@@ -0,0 +1,11 @@
+ fun main (args: Array<String>) {
+
+     println("Enter your name")
+     var name = readLine()
+     println("Hello, " + name)
+
+     println("Where are you come from ?")
+     var comeFrom = readLine()
+     println(name + " from " + comeFrom)
+
+ }
